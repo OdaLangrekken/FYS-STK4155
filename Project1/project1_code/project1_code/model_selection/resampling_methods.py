Fixed boostrap error and added bias variance decomp

Project1/project1_code/project1_code/model_selection/resampling_methods.py
@@ -83,6 +83,7 @@ def make_bootstrap_sample(X, z, sample_size=1):
     X_test (dataframe): input data not sampled, used as test data
     z_test (dataframe): output data corresponding to input data not sampled
     """
+    X = X.reset_index(drop=True)
     # Randomly draw n rows from design matrix X with replacement
     X_sample = X.sample(n=sample_size*len(X), replace=True)
     rows_chosen = X_sample.index
@@ -90,12 +91,12 @@ def make_bootstrap_sample(X, z, sample_size=1):
     z_sample = z[rows_chosen]
     
     # Use rows not sampled as test set
-    X_test = X[~X.index.isin(rows_chosen)]
-    z_test = np.delete(z, rows_chosen)
+    #X_test = X[~X.index.isin(rows_chosen)]
+    #z_test = np.delete(z, rows_chosen)
     
-    return X_sample, z_sample, X_test, z_test
+    return X_sample, z_sample
 
-def bootstrap(X, z, num_iterations=5, sample_size=1):
+def bootstrap(model, X_train, z_train, X_test, z_test, num_iterations=5, sample_size=1):
     """
     Function that uses bootstrap resampling to compute test and train mean squared error.
     For each iteration a n datapoints (determined by sample_size) are sampled from X and z.
@@ -122,15 +123,14 @@ def bootstrap(X, z, num_iterations=5, sample_size=1):
     
     # Do sampling n times
     for i in range(num_iterations):
-        X_sample, z_sample, X_test, z_test = make_bootstrap_sample(X, z, sample_size)
+        X_sample, z_sample = make_bootstrap_sample(X_train, z_train, sample_size=1)
         
         # Train the model
-        lm = LinearModel()
-        lm.fit(X_sample, z_sample)
+        model.fit(X_sample, z_sample)
         
         # Make predictions
-        z_sample_predict = lm.predict(X_sample)
-        z_test_predict = lm.predict(X_test)
+        z_sample_predict = model.predict(X_sample)
+        z_test_predict = model.predict(X_test)
         
         # Compute mean squared error for fold
         mse_test.append(MSE(z_test, z_test_predict))

Project1/project1_code/project1_code/test/test_resampling_methods.py
@@ -7,12 +7,10 @@ import pandas as pd
 
 class TestResamplingMethods(unittest.TestCase):
 
-    def test_bootstrap_trainvstest_indices(self):
-
-        # Test that test set only contains the data not sampled
+    def test_bootstrap_same_sample_size(self):
         print('-------------------------------------------------------------------')
         print('-------------------------------------------------------------------')
-        print('Bootstrap: Test that test set only contains the data not sampled')
+        print('Bootstrap: Test that X_samle and z_sample have same size')
         print('-------------------------------------------------------------------')
         # Make data
         x = np.random.uniform(0, 1, 20)
@@ -20,34 +18,12 @@ class TestResamplingMethods(unittest.TestCase):
         z = FrankeFunction(x, y)
         X = create_design_matrix(x, y, 5)
 
-        X_sample, z_sample, X_test, z_test = make_bootstrap_sample(X, z)
+        X_sample, z_sample = make_bootstrap_sample(X, z)
         
-        sample_rows = X_sample.index.unique().sort_values().tolist()
-        test_rows = X_test.index.sort_values().tolist()
+        print(f'Size of X_sample: {len(X_sample)}')
+        print(f'Size of z_sample: {len(z_sample)}')
 
-        print(f'Rows selected for sample: {sample_rows}')
-        print(f'Rows selected for test: {test_rows}')
-
-        for i in range(len(test_rows)):
-            self.assertFalse(test_rows[i] in sample_rows, 'Some rows are in both sample and test set!')
-
-    def test_bootstrap_same_test_size(self):
-        print('-------------------------------------------------------------------')
-        print('-------------------------------------------------------------------')
-        print('Bootstrap: Test that X_test and z_test have same size')
-        print('-------------------------------------------------------------------')
-        # Make data
-        x = np.random.uniform(0, 1, 20)
-        y = np.random.uniform(0, 1, 20)
-        z = FrankeFunction(x, y)
-        X = create_design_matrix(x, y, 5)
-
-        X_sample, z_sample, X_test, z_test = make_bootstrap_sample(X, z)
-        
-        print(f'Size of X_test: {len(X_test)}')
-        print(f'Size of z_test: {len(z_test)}')
-
-        self.assertEqual(len(X_test), len(z_test), 'X_test and z_test are not the same size!')
+        self.assertEqual(len(X_sample), len(z_sample), 'X_test and z_test are not the same size!')
 
 if __name__ == '__main__':
     